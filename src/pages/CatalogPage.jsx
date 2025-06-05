23a87db36a6c770a2a5a980070b2c7d8b79b3e6d
Maksym Novik <novik.kor@gmail.com>
2025-06-05 23:15:18 +0300

    Implement URL synced filters (#2)
    * feat: sync filters with URL and update docs
    
    * fix: handle 404 error in trucksOperations to return an empty array

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import Button from '../components/Button';
 import TrucksList from '../components/TrucksList';
-import { selectError, selectIsLoading } from '../redux/trucks/selectors.js';
+import { selectError, selectIsLoading, selectTrucks } from '../redux/trucks/selectors.js';
 import Filters from '../components/Filters.jsx';
 import Loader from '../components/Loader';
 import { incrementPage } from '../redux/filters/slice.js';
@@ -10,6 +10,7 @@ const CatalogPage = () => {
   const dispatch = useDispatch();
   const isLoading = useSelector(selectIsLoading);
   const error = useSelector(selectError);
+  const trucks = useSelector(selectTrucks);
 
   const handleLoadMore = (event) => {
     dispatch(incrementPage());
@@ -33,12 +34,16 @@ const CatalogPage = () => {
             {error && <p>{error}</p>}
             <TrucksList />
 
-            {!isLoading && (
+            {!isLoading && trucks.length === 0 && (
+              <p className="text-center mt-[20px]">Sorry, nothing matches your filters</p>
+            )}
+
+            {!isLoading && trucks.length > 0 && (
               <div className="flex items-center justify-center mt-[20px] lg:mt-[30px]">
                 <Button
                   onClick={handleLoadMore}
                   buttonLabel="Load more"
-                  className="min-w-[173px] max-w-[250px] py-[16px] px-[48px] tracking-[-0.08px] leading-[1.5em] text-center text-textPrimary bg-bgPrimaryColor rounded-[200px] border border-borderButtonColor inline-flex items-center justify-center  hover:bg-linear-45 hover:from-[#dadde1] from-40% hover:to-[#ffc531] to-90% outline-0 focus:ring-2 focus:ring-green-500/50 focus:bg-ButtonHoverColor focus:shadow-lg focus:shadow-green-500/50 transition-colors duration-300 ease-in "
+                  className="min-w-[173px] max-w-[250px] py-[16px] px-[48px] tracking-[-0.08px] leading-[1.5em] text-center text-textPrimary bg-bgPrimaryColor rounded-[200px] border border-borderButtonColor inline-flex items-center justify-center  hover:bg-linear-45 hover:from-[#dadde1] from-40% hover:to-[#ffc531] to-90% outline-0 focus:ring-2 focus:ring-green-500/50 focus:bg-ButtonHoverColor focus:shadow-lg focus:shadow-green-500/50 transition-colors duration-300 ease-in"
                 />
               </div>
             )}
