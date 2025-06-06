a22a82602ed8cfefd216ab0954399170afbd9d38
Max Novik <novik.kor@gmail.com>
2025-06-06 03:01:12 +0300

    Fixed pagination broken by the loader

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -27,36 +27,34 @@ const CatalogPage = () => {
 
   return (
     <section className="px-[12px] pt-[12px] pb-[40px]  lg:px-[64px] lg:pt-[48px] lg:pb-[50px]">
-      {/*! ========== section's container ========== */}
       <div className="flex flex-row w-full">
         <aside className="hidden lg:block max-w-[488px]  ">
           <Filters />
         </aside>
 
-        {isLoading ? (
-          <div className="w-full relative">
-            <Loader />
-          </div>
-        ) : (
-          <div className="max-w-[936px]">
-            {error && <p>{error}</p>}
-            <TrucksList />
+        <div className="max-w-[936px]">
+          {error && <p>{error}</p>}
+          <TrucksList />
 
-            {!isLoading && trucks.length === 0 && (
-              <p className="text-center mt-[20px]">Sorry, nothing matches your filters</p>
-            )}
+          {!isLoading && trucks.length === 0 && (
+            <p className="text-center mt-[20px]">Sorry, nothing matches your filters</p>
+          )}
 
-            {!isLoading && hasMore && trucks.length > 0 && (
-              <div className="flex items-center justify-center mt-[20px] lg:mt-[30px]">
-                <Button
-                  onClick={handleLoadMore}
-                  buttonLabel="Load more"
-                  className="min-w-[173px] max-w-[250px] py-[16px] px-[48px] tracking-[-0.08px] leading-[1.5em] text-center text-textPrimary bg-bgPrimaryColor rounded-[200px] border border-borderButtonColor inline-flex items-center justify-center  hover:border-borderButtonHoverColor"
-                />
-              </div>
-            )}
-          </div>
-        )}
+          {!isLoading && hasMore && trucks.length > 0 && (
+            <div className="flex items-center justify-center mt-[20px] lg:mt-[30px]">
+              <Button
+                onClick={handleLoadMore}
+                buttonLabel="Load more"
+                className="min-w-[173px] max-w-[250px] py-[16px] px-[48px] tracking-[-0.08px] leading-[1.5em] text-center text-textPrimary bg-bgPrimaryColor rounded-[200px] border border-borderButtonColor inline-flex items-center justify-center  hover:border-borderButtonHoverColor"
+              />
+            </div>
+          )}
+          {isLoading && (
+            <div className="w-full relative">
+              <Loader />
+            </div>
+          )}
+        </div>
       </div>
     </section>
   );
